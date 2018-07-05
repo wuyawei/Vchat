3125f8e231a12b329ce539c0f4add0bb4c7be027
wyw <2499115366@qq.com>
2018-07-05 23:50:24 +0800

    暂时添加fetch、axios两种请求方式

--- chatRoom/src/components/login.vue
+++ chatRoom/src/components/login.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         name: 'login',
         username: '',
@@ -56,11 +57,14 @@
             }
         },
         mounted() {
-            fetch('/api/getUser').then(res=>res.json()).then(r=>{
-                if(r.code === 0){
-                    console.log(r);
-                }
-            });
+//            fetch('/api/getUser').then(res=>res.json()).then(r=>{
+//                if(r.code === 0){
+//                    console.log(r);
+//                }
+//            });
+//            axios.get('/api/getUser').then(r => {
+//                console.log(r);
+//            })
         }
     }
 </script>

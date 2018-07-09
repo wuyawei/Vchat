2c83c7549c73899bb1635f5f936ca5f978de17ae
wyw <2499115366@qq.com>
2018-07-09 22:31:42 +0800

    引入vuex

--- chatRoom/src/main.js
+++ chatRoom/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueSocketio from 'vue-socket.io';
@@ -13,8 +14,9 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    store,
+    components: {App},
+    template: '<App/>'
+});

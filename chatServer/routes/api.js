94cb32ce4915a1b8a8803774386e8f6e63949173
wyw <2499115366@qq.com>
2018-07-26 23:59:18 +0800

    第二种上传方案

--- chatServer/routes/api.js
+++ chatServer/routes/api.js
@@ -1,12 +1,14 @@
 const api = require('../controller/apiList');
-const multer=require('multer');
+// const multer=require('multer'); // 文件上传 第一种上传方案所需
 const fs=require('fs');
 const os=require('os');
 const path=require("path");
 
 let express = require('express');
 let router = express.Router();
-let upload = multer({dest: './public/uploads/'}); // os.tmpdir() 跨磁盘有权限问题
+// let upload = multer({dest: './public/uploads/'}); // os.tmpdir() 跨磁盘有权限问题 第一种上传方案所需
+
+let uploads = require('../utils/upload'); // 第二种上传所需
 
 router.get('/getUser', api.getUser); // 获取用户、测试接口
 router.post('/login', api.login); // 登录
@@ -15,26 +17,33 @@ router.post('/loginOut', api.loginOut); // 退出
 router.post('/getUserInfo', api.getUserInfo); // 登录状态获取用户信息
 
 // 新路径要写真实路径，文件夹不存在multer不会主动创建
-const uploadInmage = (req, res)=>{
-    fs.rename(
-        req.file.path,
-        path.join('./public/uploads/' + Date.now() + '-' + req.file.originalname),
-        (err)=>{
-            if (err) {
-                res.json({
-                    code: -1,
-                    data: '上传失败'
-                });
-                throw err;
-            }
-            res.json({
-                code: 0,
-                data: '/uploads/' + Date.now() + '-' + req.file.originalname
-            });
-        }
-    );
-};
+// const uploadInmage = (req, res)=>{ // 第一种上传方案所需
+//     fs.rename(
+//         req.file.path,
+//         path.join('./public/uploads/' + Date.now() + '-' + req.file.originalname),
+//         (err)=>{
+//             if (err) {
+//                 res.json({
+//                     code: -1,
+//                     data: '上传失败'
+//                 });
+//                 throw err;
+//             }
+//             res.json({
+//                 code: 0,
+//                 data: '/uploads/' + Date.now() + '-' + req.file.originalname
+//             });
+//         }
+//     );
+// };
 
-router.post('/uploadInmage', upload.single('f'), uploadInmage);
+// f  前端文件上传name必须为f
+// router.post('/uploadInmage', upload.single('f'), uploadInmage); // 第一种上传方案所需
+router.post('/uploadInmage', uploads.single('f'), (req, res) => { // 第二种上传方案
+    res.json({
+        code: 0,
+        data: '/uploads/' + req.file.filename
+    });
+});
 
 module.exports = router;

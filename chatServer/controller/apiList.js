79d6a72b41fbf960dcee1367b31be6ec10b5115e
wuyw <w2499115366@126.com>
2018-10-01 21:49:13 +0800

    升级本地node版本以支持es6语法

--- chatServer/controller/apiList.js
+++ chatServer/controller/apiList.js
@@ -1,31 +1,7 @@
 let users = require('./user');
 let groups = require('./group');
-let {
-    getUser,
-    login,
-    upUserInfo,
-    signUp,
-    loginOut,
-    getUserInfo,
-    getUserDetail
-} = users;
-let {
-    createGroup,
-    getMyGroup,
-    getGroupUsers,
-    huntGroups
-} = groups;
 
 module.exports = {
-    getUser,
-    login,
-    upUserInfo,
-    signUp,
-    loginOut,
-    getUserInfo,
-    getUserDetail,
-    createGroup,
-    getMyGroup,
-    getGroupUsers,
-    huntGroups
+    ...users,
+    ...groups
 };

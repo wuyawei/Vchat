fbce7f61bf93c0987c112f70ce9eb2043275948d
wuyw <w2499115366@126.com>
2018-12-02 21:26:07 +0800

    上传文件按日期分类

--- chatServer/routes/api.js
+++ chatServer/routes/api.js
@@ -10,7 +10,7 @@ const os=require('os');
 const path=require("path");
 // let upload = multer({dest: './public/uploads/'}); // os.tmpdir() 跨磁盘有权限问题 第一种上传方案所需
 const uploads = require('../utils/upload'); // 上传js
-
+const utils = require('../utils/utils');
 
 // 新路径要写真实路径，文件夹不存在multer不会主动创建
 // const uploadInmage = (req, res)=>{ // 第一种上传方案所需
@@ -36,9 +36,10 @@ const uploads = require('../utils/upload'); // 上传js
 // f  前端文件上传name必须为f
 // router.post('/uploadInmage', upload.single('f'), uploadInmage); // 第一种上传方案所需
 router.post('/uploadFile', uploads.uploadFile.single('f'), (req, res) => { // 第二种上传方案
+    let date = utils.formatTime(new Date()).split(' ')[0];
     res.json({
         code: 0,
-        data: '/uploads/' + req.file.filename
+        data: '/uploads/' + date + '/' + req.file.filename
     });
 });
 
--- chatServer/utils/upload.js
+++ chatServer/utils/upload.js
@@ -1,8 +1,13 @@
+const utils = require('./utils');
+const fs=require('fs');
 let  multer=require('multer');
 let storage = multer.diskStorage({
-    //设置上传后文件路径，uploads文件夹会自动创建。
+    //设置上传后文件路径， // 路径写成函数需要自己创建文件夹，字符串会自动创建。
     destination: function (req, file, cb) {
-        cb(null, './public/uploads')
+        let date = utils.formatTime(new Date()).split(' ')[0];
+        let path = './public/uploads/' + date;
+        fs.mkdirSync(path);
+        cb(null, path)
     },
     //给上传文件重命名，获取添加后缀名
     filename: function (req, file, cb) {

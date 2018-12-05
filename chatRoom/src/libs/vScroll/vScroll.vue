feebf4f7c2b48e8a8920eb8fac4c971d3c8b529e
wuyw <wuyw@zonekey.com.cn>
2018-12-05 16:59:38 +0800

    增加滚动加载组件

---
+++ chatRoom/src/libs/vScroll/vScroll.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="vchat-scroll" v-loadMore="loadMore" :data-scrollWay="scrollWay" :data-nodata="nodata">
+        <slot></slot>
+        <span id="loadmoreIcon" style="overflow: hidden" v-if="nodata"><i class="el-icon-loading"></i></span>
+        <span v-else>没有更多数据了</span>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'vScroll',
+        props: {
+            scrollWay: { // up 、down
+                type: String,
+                default: 'down'
+            },
+            loadMore: {
+                type: Function,
+                default: function() {
+                    return function() {};
+                }
+            },
+            nodata: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                scroll: null
+            }
+        },
+        directives: {
+            'loadMore': {
+                inserted(el, binding) {
+                    let loadmoreIcon = document.getElementById('loadmoreIcon');
+                    loadmoreIcon.style.display = 'none';
+                    el.dataset.flag = 'true';
+                    function scroll() {
+                        /*
+                             * scrollHeight 获取元素内容高度(只读)
+                             * scrollTop 获取或者设置元素的偏移值,常用于, 计算滚动条的位置, 当一个元素的容器没有产生垂直方向的滚动条, 那它的scrollTop的值默认为0.
+                             * clientHeight 读取元素的可见高度(只读)
+                             * 如果元素滚动到底, 下面等式返回true, 没有则返回false:
+                             * ele.scrollHeight - ele.scrollTop === ele.clientHeight;
+                             */
+                        if (el.dataset.nodata === 'true') {
+                            el.removeEventListener('scroll', scroll)
+                        }
+                        let CONDITION;
+                        if (el.dataset.scrollWay === 'up') {
+                            CONDITION = this.scrollTop <= 0;
+                        } else {
+                            CONDITION = this.scrollHeight - this.scrollTop <= this.clientHeight;
+                        }
+                        if (CONDITION && el.dataset.flag === 'true') {
+                            el.dataset.flag = 'false';
+                            loadmoreIcon.style.display = 'block';
+                            binding.value(() => {
+                                el.dataset.flag = 'true';
+                                loadmoreIcon.style.display = 'none';
+                            });
+                        }
+                    }
+                    el.addEventListener('scroll', scroll);
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .vchat-scroll{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+</style>

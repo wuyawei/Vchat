aa6b02f6d221249ca993e9e3b4b4c15eefe2af89
wuyw <w2499115366@126.com>
2019-01-03 21:05:01 +0800

    修改加载进度条层级

--- chatRoom/src/router/index.js
+++ chatRoom/src/router/index.js
@@ -5,6 +5,7 @@
 import router from './router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css'; //这个样式必须引入
+import './nprogress.scss';
 // NProgress.inc(0.2);
 NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false });
 

5b1a4f519778114d4c90ae03b974b4f3cfcceb30
wuyw <w2499115366@126.com>
2019-01-09 21:51:35 +0800

    日程管理接口

--- chatServer/model/apiModel.js
+++ chatServer/model/apiModel.js
@@ -3,11 +3,13 @@ let groups = require('./group');
 let messages = require('./messages');
 let friendly = require('./friendly');
 let expression = require('./expression');
+let todo = require('./todo');
 
 module.exports = {
     ...users,
     ...groups,
     ...messages,
     ...friendly,
-    ...expression
+    ...expression,
+    ...todo
 };
---
+++ chatServer/model/todo.js
@@ -0,0 +1,60 @@
+/**
+ * Created by wyw on 2019/1/9.
+ */
+const db = require('../utils/database');
+let todo = db.model("todo", {
+    title: String, // 活动名称
+    address: String, // 活动地点
+    start: { type: Date, default: Date.now() }, // 开始时间
+    end: { type: Date, default: Date.now() }, // 结束时间
+    userName: String, // 用户名
+    way: String // 活动提醒方式
+});
+
+const getTodoList = (params, callback) => { // 查找我的日程
+    todo.find(params).then(r => {
+        callback({code: 0, data: r});
+    }).catch(err => {
+        callback({code: -1, data: '查找失败'});
+    })
+};
+
+const addTodo = (params, callback) => { // 新增日程
+    todo.create(params).then(r => {
+        if (r['_id']) {
+            callback({code: 0, data: r['_id']});
+        } else {
+            callback({code: -1, data: '添加失败'});
+        }
+    }).catch(err => {
+        callback({code: -1, data: '添加失败'});
+    })
+};
+
+const upTodo = (params, callback) => { // 编辑日程
+    todo.update({'_id': params['_id']}, params).then(r => {
+        if (raw.nModified > 0) {
+            callback({code: 0});
+        } else {
+            callback({code: -1});
+        }
+    }).catch(err => {
+        callback({code: -1, data: '编辑失败'});
+    })
+};
+
+const delTodo = (params, callback) => { // 删除日程
+    todo.remove({'_id': params['_id']}).then(r => {
+        console.log(r);
+        callback({code: 0, data: r});
+    }).catch(err => {
+        callback({code: -1, data: '删除失败'});
+    })
+};
+
+module.exports = {
+    getTodoList,
+    addTodo,
+    upTodo,
+    delTodo
+};

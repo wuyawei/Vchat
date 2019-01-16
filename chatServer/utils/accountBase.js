a020aff8b57aabd2f65e89ae045e834d80ae0b76
wuyw <wuyw@zonekey.com.cn>
2019-01-16 10:41:49 +0800

    初始化操作后自动退出命令行

--- chatServer/utils/accountBase.js
+++ chatServer/utils/accountBase.js
@@ -12,6 +12,7 @@ let accountBase = db.model("accountBase", {
     random: Number // (随机数索引，用于随机查找一条)
 });
 let initAccount = () => {
+    let index = 0;
     for (let i = 10000001; i <= 10001999; i++) { // 首批用户默认账号 8位
         let account = new accountBase({ // 通过new Model1()创建的文档doc1，必须通过save()方法，才能将创建的文档保存到数据库的集合中
             code: i,
@@ -35,12 +36,18 @@ let initAccount = () => {
             random: Math.random()
         });
         account.save(function (error, newAccount) { // 回调函数是可选项，第一个参数为err，第二个参数为保存的文档对象
+            index = i;
             if (error) {
                 console.log("插入 " + account + " 错误:" + error);
             } else {
                 console.log("已成功插入 " + newAccount);
             }
         });
+    }
+    if (index === 100999) {
+        setInterval(_ => {
+            process.exit(); // 退出进程
+        }, 1000)
     }
 };
 module.exports = {
